46a7c198ac6c2a51f7e529c5b63baa723903c1e5
stephen foerster <stephen.foerster1@gmail.com>
2026-02-18 16:15:03 +0100

    Fix deployment

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+const isGithubPages = process.env.DEPLOY_TARGET === 'github';
+
 export default defineConfig({
   plugins: [vue()],
-  base: '/hearts_game/',
+  base: isGithubPages ? '/hearts_game/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
