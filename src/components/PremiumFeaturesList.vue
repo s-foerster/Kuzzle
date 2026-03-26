b964d10c256be544df23df7761913145290ee8e8
stephen foerster <stephen.foerster1@gmail.com>
2026-03-26 14:03:12 +0100

    Add PremiumFeaturesList component missing from repo

---
+++ src/components/PremiumFeaturesList.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul :class="listClass">
+    <li>
+      ✨ Débloquez l'historique de plus de 1000 puzzles de logique en illimité
+    </li>
+    <li>📊 Accédez à vos statistiques détaillées</li>
+    <li>🏆 Comparez-vous aux autres joueurs via le classement</li>
+    <li>🎨 Débloquez l'accès aux thèmes exclusifs</li>
+    <li>💛 Soutenir le développement de Rubih Games</li>
+  </ul>
+</template>
+
+<script setup>
+defineProps({
+  listClass: {
+    type: String,
+    default: "",
+  },
+});
+</script>
